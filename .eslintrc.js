16f7cb7ad5adf49c82cfa407d1ef2f07fd569b8d
Collin Arnold <collin@collinarnold.net>
2018-03-08 19:26:52 -0800

    updating eslint configuration to validate the tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
     extends: 'eslint:recommended',
+    plugins: [
+        'jest'
+    ],
+    parserOptions: {
+        ecmaVersion: 8,
+    },
     env: {
         browser: true,
         es6: true,
+        node: true,
+        'jest/globals': true,
     },
     globals: {
         RingtailSDK: false,
